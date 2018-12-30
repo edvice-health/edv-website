ee24cfb3966eb1d307dd76e4f7549e65351a5979
Maria Kuzovchikova <iseesomuch@gmail.com>
2018-12-30 20:09:08 +0100

    Added bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { Section, Hero, Container, Heading } from 'react-bulma-components'
 import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>ABOUT EDVICE</h1>
-    <p>The first platform to connect patiensts and eating disorders specialists worldwide.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Section>
+        <Hero>
+          <Hero.Body>
+            <Container>
+              <Heading>
+                ABOUT EDVICE
+              </Heading>
+              <Heading subtitle size={4}>
+                The first platform to connect patiensts and eating disorders specialists worldwide.
+              </Heading>
+            </Container>
+          </Hero.Body>
+        </Hero>
+    </Section>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
